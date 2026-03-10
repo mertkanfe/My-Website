1caf6da9c28d76bd42d60204375bdc861c5282d9
Mert <mertkanfe@hotmail.com>
2026-03-10 22:54:23 +0300

    Dark mode özelliği eklendi

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// Menu
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
+}
+
+// Dark mode add
+const darkModeIcon = document.querySelector('#dark-mode-icon');
+const darkModeIconInner = document.querySelector('#dark-mode-icon i');
+
+darkModeIcon.onclick = () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeIconInner.classList.replace('fa-moon', 'fa-sun');
+        darkModeIconInner.style.color = "#f1c40f";
+    } else {
+        darkModeIconInner.classList.replace('fa-sun', 'fa-moon');
+        darkModeIconInner.style.color = "white";
+    }
 }
